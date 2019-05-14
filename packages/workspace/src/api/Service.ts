25444ff1899485d798f4d2612f27d8e9f01e3809
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-05-14 14:56:24 +0300

    style fix

--- packages/workspace/src/api/Service.ts
+++ packages/workspace/src/api/Service.ts
@@ -1,6 +1,6 @@
 import { ServiceDefinition } from '@scalecube/scalecube-microservice/lib/api';
 
- export default interface Service {
+export default interface Service {
   serviceName: string;
   path: string;
   definition: ServiceDefinition;
