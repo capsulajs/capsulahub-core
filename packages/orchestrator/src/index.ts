a12e8c350c621075bf3a7b250f45978ed845aa59
Stephane Benayoun <stephane.benayoun@jivygroup.com>
2019-05-15 15:27:52 +0300

    Remove global variables

--- packages/orchestrator/src/index.ts
+++ packages/orchestrator/src/index.ts
@@ -1,8 +1,3 @@
-import { Workspace } from '@capsulajs/capsulahub-core-workspace/api';
-import { OrchestratorConfig } from './api';
 import * as api from './api';
-
-declare var CAPSULAHUB_WORKSPACE: Workspace;
-declare var SERVICE_CONFIG: OrchestratorConfig;
 
 export { api };
