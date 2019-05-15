5e56d576c279fa953a075b133aaecdabb47a7cf1
MikeSquall <michael.arbib@om2.com>
2019-05-15 20:41:38 +0300

    Test scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/env bash
 S3_PATH="s3://$S3_BUCKET"
 SERVICE=$1
-SERVICE_PATH=$(bash -c "../../scripts/path.sh $SERVICE")
+
+echo "*********************\n$SERVICE\n*******************"
+
+define_path() {
+    SLUG="/error/"
+
+    # PR
+    if [[ ! "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+        SLUG="/PR/$TRAVIS_PULL_REQUEST_BRANCH/"
+    # master # develop
+    elif [[ "$TRAVIS_BRANCH" == "develop" ]]; then
+        SLUG="/$TRAVIS_BRANCH/"
+    elif [[ "$TRAVIS_BRANCH" == "master" ]]; then
+        SLUG="/rc/"
+    fi;
+
+    if [[ "$SLUG" == "/error/" ]]; then
+        exit 1
+    fi;
+
+    return "$SERVICE/$SLUG"
+}
+
+SERVICE_PATH=$(define_path)
+
 
 echo "current branch: $TRAVIS_BRANCH $TRAVIS_PULL_REQUEST_BRANCH is pull request: $TRAVIS_PULL_REQUEST"
 echo "S3 Path: $S3_PATH"
