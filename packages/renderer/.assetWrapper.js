d31e1471757d1831e6d10e06f4824ac194d31942
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-05-22 12:58:10 +0300

    update assets wrapper

--- packages/renderer/.assetWrapper.js
+++ packages/renderer/.assetWrapper.js
@@ -6,8 +6,8 @@ const PACKAGE = require(path.join(CWD, 'package.json'));
 const varRequire = async ({ name, bundler }) => {
   if (name.split('.').pop() === 'js' && bundler.options.production) {
     return {
-      header: `var require = window.require = false; var publicExports={};(function(){"use strict";var parcelRequire=undefined;`,
-      footer: `})();export default publicExports;`,
+      header: `var require = window.require = false; var publicExports={}; var API = {}; (function(){"use strict";var parcelRequire=undefined;`,
+      footer: `})();export API; export default publicExports;`,
     };
   }
 };
--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,9 +1,13 @@
 import bootstrap from './RendererService';
+import * as apiMap from './api';
 
 // @ts-ignore
 if (typeof publicExports !== 'undefined') {
   // @ts-ignore
+  API = apiMap;
+  // @ts-ignore
   publicExports = bootstrap;
 }
 
+export const api = apiMap;
 export default bootstrap;
