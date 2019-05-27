81319ffe8207d7434838cddcd144a3ed949fe5c4
dmitriyslaym <dmitriyslaym@gmail.com>
2019-05-27 12:37:55 +0300

    Fixed Catalog component

--- packages/externalModules/src/components/Catalog.tsx
+++ packages/externalModules/src/components/Catalog.tsx
@@ -1,18 +1,84 @@
-const bootstrap = (workspace) => {
-  return new Promise((resolve) => {
-    class Catalog extends HTMLElement {
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import { Observable, from } from 'rxjs';
+import { map, switchMap, startWith, mergeMap } from 'rxjs/operators';
+import { Catalog } from '@capsulajs/capsulahub-ui';
+import { dataComponentHoc } from '../helpers/dataComponentHoc';
+import { mapServiceMethods } from '../helpers/mapServiceMethods';
+
+const bootstrap = (WORKSPACE: any) => {
+  return new Promise(async (resolve) => {
+    interface MethodCatalogProps {
+      methods: any[];
+    }
+
+    interface MethodCatalogState {
+      selectedMethod?: object;
+    }
+
+    class MethodCatalogUI extends React.Component<MethodCatalogProps, MethodCatalogState> {
+      public state = {
+        selectedMethod: undefined,
+      };
+
+      public render() {
+        const { selectedMethod } = this.state;
+        const { methods } = this.props;
+
+        if (methods.length === 0) {
+          return 'No services ..';
+        }
+
+        return (
+          <Catalog
+            methods={mapServiceMethods(methods)}
+            selectedMethod={selectedMethod}
+            selectMethod={this.handleOnChange}
+          />
+        );
+      }
+
+      private handleOnChange = (selectedMethod) => this.setState({ selectedMethod });
+    }
+
+    const mountPoint = 'web-method-catalog';
+
+    class MethodCatalog extends HTMLElement {
+      public props$?: Observable<any>;
+
       constructor() {
         super();
-        workspace.services({}).then(({ EnvRegistry }) => {
-          EnvRegistry.then(({ proxy }) => {
-            proxy.methods$().subscribe((methods) => console.log(methods));
-          });
-        });
-        this.innerHTML = '<div>I am the catalog</div>';
+        this.innerHTML = `<div id=${mountPoint}></div>`;
+      }
+
+      public connectedCallback() {
+        const Component: any = this.props$ ? dataComponentHoc(MethodCatalogUI, this.props$) : MethodCatalogUI;
+        ReactDOM.render(<Component />, document.getElementById(mountPoint));
       }
     }
 
-    resolve(Catalog);
+    class CatalogWithData extends MethodCatalog {
+      public setProps() {
+        console.log('goes to setProps');
+
+        WORKSPACE.services({}).then((services) => {
+          console.log('services', services);
+        });
+
+        this.props$ = from(WORKSPACE.services({})).pipe(
+          mergeMap((services: any) => from(services.EnvRegistry)),
+          map((serviceData: any) => serviceData.proxy),
+          switchMap((envRegistry) => {
+            return envRegistry.methods$({}).pipe(map((methods) => ({ methods })));
+          }),
+          startWith({
+            methods: [],
+          })
+        );
+      }
+    }
+
+    resolve(CatalogWithData);
   });
 };
 
