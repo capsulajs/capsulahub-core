9b14b9f426017e627ad34d31486aa52974588506
mikearbib <arbib.michael@gmail.com>
2019-05-27 16:33:14 +0300

    Fixed comments generator

--- scripts/gh.sh
+++ scripts/gh.sh
@@ -7,22 +7,16 @@ link="[**S3**](${URL}index.js)"
 
 commentAlreadyExists() {
     comments=$(curl -s -u "$GH_USER:$GH_ACCESS_TOKEN" "$COMMENTS_URL" | jq -r '.[].body')
-    echo ${comments}
+    echo "comments --> $comments"
+    [[ -z "$comments" ]] && echo "no comments" && return 1
     [[ "$comments" == *"$1"* ]]
     return $?
 }
 
 comment(){
-    COMMENT_TEXT="""
-**Travis-CI**
-<table>
-    <tr>
-        <td><h2>$SERVICE</h2><br />deployed on $link</td>
-    </tr>
-</table>
-"""
-
+    COMMENT_TEXT="**Travis-CI**<table><tr><td><h2>$SERVICE</h2><br />deployed on $link</td></tr></table>"
     echo "$COMMENT_TEXT"
+
     # Post comment about service if it's not posted yet
     commentAlreadyExists "$COMMENT_TEXT" && echo "Comment already posted" || \
     curl -d '{"body":"'"$COMMENT_TEXT"'"}' -u "$GH_USER:$GH_ACCESS_TOKEN" -X POST "$COMMENTS_URL"
